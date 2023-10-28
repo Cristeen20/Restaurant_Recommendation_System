80311477d4955ebffaf2ec74c18195df1f7b13f9
Cristeen20 <cristeena121@gmail.com>
2023-10-27 20:46:19 -0400

    user scrap

--- user_link_scrap.py
+++ user_link_scrap.py
@@ -2,6 +2,8 @@ import json
 import asyncio
 from pyppeteer import launch
 
+from user_page_scrap import scrap_user
+from csv_write import write_csv
 
 async def scrape_website(url):
 
@@ -38,25 +40,30 @@ async def user_scrap(review_urls):
         # Navigate to the URL you want to scrape
         await page.goto(review)
 
+        user_div_name = 'ui_avatar.resp'
+        await page.waitForSelector(f'div.{user_div_name}')
+    
+        await page.click(f'div.{user_div_name}')
+        await page.waitForSelector('div.memberOverlayRedesign.g10n')
+        user_div = await page.querySelector('div.memberOverlayRedesign.g10n')
+        user_href = await user_div.querySelector('a')
+        url = await page.evaluate('(element) => element.getAttribute("href")', user_href)
 
-        await page.waitForSelector('div.info_text.pointer_cursor')
-        divElements = await page.querySelectorAll('div.info_text.pointer_cursor')
-        
-        print(review)
-        for div_element in divElements:
-            profile_name = await page.evaluate('(element) => element.textContent', div_element)
-            
-            if " " not in profile_name:
-                url = "https://www.tripadvisor.ca/Profile/"+profile_name
-                user_urls.append(url)
+        print(url)
+        user_url = 'https://www.tripadvisor.ca' + url
+        user_urls.append(user_url)
 
-        
         await browser.close()
+        
         
 
     return user_urls
 
 # Run the scraping function
-main_url = "https://www.tripadvisor.ca/Restaurants-g155019-Toronto_Ontario.html#REVIEWS"
+#https://www.tripadvisor.ca/Restaurants-g155019-Toronto_Ontario.html
+#https://www.tripadvisor.ca/Restaurants-g181740-Richmond_Hill_Ontario.html
+main_url = "https://www.tripadvisor.ca/Restaurants-g181720-Markham_Ontario.html#REVIEWS"
 review_urls = asyncio.get_event_loop().run_until_complete(scrape_website(main_url))
 profile_urls = asyncio.get_event_loop().run_until_complete(user_scrap(review_urls))
+user_json = asyncio.get_event_loop().run_until_complete(scrap_user(profile_urls))
+write_csv(user_json,"User_data.csv")
